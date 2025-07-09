eeca3938d606112323be1b438ff807e80261ef3d
rajiladyboss <manidhidigitalsolutions@gmail.com>
2025-07-09 07:53:22 +0530

    Add files via upload

---
+++ Day1_PersonalGreeting.py
@@ -0,0 +1,7 @@
+# Ask for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+color = input("What is your favorite color? ")
+
+# Create a personalized message
+print(f"\nHi {name}! At {age} years old, loving {color} makes you uniquely awesome!")
